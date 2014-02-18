73356f8d2a9c0a41e3d3c41ac842cff09c5cdea0
Jarrett Cruger <jcrugzz@gmail.com>
2014-02-17 23:42:10 -0500

    [test] add backoff test as well just for sanity

--- test/retry-test.js
+++ test/retry-test.js
@@ -22,4 +22,25 @@ test('test request response when using 2 retries functionality and failing child
     t.fail(msg, 'We should not receive a message with a determinstically failing child process');
     t.end();
   })
-})
+});
+
+test('retry twice with backoff enabled', function (t) {
+  t.plan(3);
+
+  var message = { action: 'woooo', doc: 'document' };
+  fork({
+    path: child,
+    retries: 2,
+    backoff: true
+  })
+  .fork(message)
+  .on('error', function (err) {
+    t.ok(err, 'Failed after attempting retry with ' + err.message);
+    t.end();
+  })
+  .on('retry', function (yo) { t.ok(yo, 'retry attempt') })
+  .on('response', function (msg) {
+    t.fail(msg, 'We should not receive a message with a determinstically failing child process');
+    t.end();
+  })
+});
